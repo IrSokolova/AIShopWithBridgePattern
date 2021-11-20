9a20ded82880aba5f1eddcedf4576694218a775a
IrSokolova <i.sokolova@innopolis.university>
2021-11-20 15:21:03 +0300

    Красивы

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -2,9 +2,6 @@ package com.company;
 
 public class Printing implements Strategy{
 
-    public Printing() {
-    }
-
     @Override
     public void execute(AIShop aiShop) {
         aiShop.printProducts();
